25b78444bc8e60f06b094f1a452cad645da63ff2
nisafhh <nisafauziyyahhandari>
2023-05-21 12:17:38 +0700

    saya ubah lagi MainActivity.kt nyaa

--- app/src/main/java/com/example/assesment1_nisafauziyyah/MainActivity.kt
+++ app/src/main/java/com/example/assesment1_nisafauziyyah/MainActivity.kt
@@ -6,65 +6,31 @@ import android.content.DialogInterface
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
-import com.example.assesment1_nisafauziyyah.databinding.ActivityMainBinding
-import java.text.DecimalFormat
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
-    lateinit var selectedUnit: String
+    class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        val df = DecimalFormat("#.##")//Decimal formatter
-        selectedUnit = "Fahrenheit"
+        class MainActivity : AppCompatActivity() {
 
-        binding.pilihTipe.setOnClickListener() {
-            showAlertDialog()
+            private lateinit var navController: NavController
 
-        }
+            override fun onCreate(savedInstanceState: Bundle?) {
+                super.onCreate(savedInstanceState)
 
-        binding.EditInput.addTextChangedListener() {
-            val resultText: String
-            var inputVal = binding.EditInput.text.toString()
-            if (inputVal.isNotEmpty()) {
-                var doubleInput = inputVal.toDouble()
-                if (selectedUnit == "Fahrenheit") {
-                    resultText = df.format((doubleInput - 32) * 5 / 9)
-                    binding.textResultType.text = "Celsius"
-                } else {
-                    //(0°C × 9/5) + 32
-                    resultText = df.format((doubleInput * 9 / 5) + 32)
-                    binding.textResultType.text = "Fahrenheit"
-                }
+                setContentView(R.layout.activity_main)
 
-                binding.textResult.text = resultText
+                navController = findNavController(R.id.myNavHostFragment)
+                NavigationUI.setupActionBarWithNavController(this, navController)
             }
 
+            override fun onSupportNavigateUp(): Boolean {
+                return navController.navigateUp()
+            }
         }
-
-    }
-
-    private fun showAlertDialog() {
-        val alertDialog: Builder = Builder(this@MainActivity)
-        alertDialog.setTitle("Select Unit") //Setting title for alertBox
-        val items = arrayOf("Fahrenheit", "Celsius") //Array to contained Options
-        val checkedItem = -1
-        alertDialog.setSingleChoiceItems(items, checkedItem,
-            DialogInterface.OnClickListener { dialog, which ->
-                selectedUnit = items[which]
-                binding.pilihText.setText(items[which])
-            })
-        alertDialog.setPositiveButton(
-            android.R.string.ok,
-            DialogInterface.OnClickListener { dialog, which ->
-                dialog.dismiss()
-            })
-        val alert: AlertDialog = alertDialog.create()
-        alert.setCanceledOnTouchOutside(false)
-        alert.show()
     }
 }
